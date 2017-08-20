53cb4844c71342c2b49d9d3d8fbcbfffadda7658
Tal Zemah <talzemah@Tals-Mac.local>
2017-08-20 20:54:36 +0300

    for sync purpose

---
+++ GameLogic.swift
@@ -0,0 +1,29 @@
+//
+//  GameLogic.swift
+//  WhackAFrog
+//
+//  Created by Tal Zemah on 20/08/2017.
+//  Copyright © 2017 Tal Zemah. All rights reserved.
+//
+
+import Foundation
+
+class GameLogic {
+    
+    
+//    var timer: Timer
+//    var score:Int = 0
+//    let life:Int = 0 // create const with number 3
+//    //var type:eType /// create enum
+//    var gameBoard: GameBoard
+//    
+//    func clickOnTile(int: i, int: j) {
+//        
+//    }
+    
+    
+    
+    
+    
+    
+}
