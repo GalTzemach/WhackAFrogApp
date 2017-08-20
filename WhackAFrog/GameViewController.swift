75cbabf33239ee4b5e6366436da92559a247ff7b
Tal Zemah <talzemah@Tals-Mac.local>
2017-08-20 22:45:25 +0300

    finish design

--- WhackAFrog/GameViewController.swift
+++ WhackAFrog/GameViewController.swift
@@ -8,14 +8,30 @@
 
 import UIKit
 
+let numOfRow: Int = 5
+let numOfCol: Int = 4 // num of element per row
+
 class GameViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
     var images: [String] = ["good", "bad", "life"]
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let screenWidth: Int = Int(UIScreen.main.bounds.width)
+        let itemSize = screenWidth / numOfCol - (30 + 40 / numOfCol) // 4 element each row
+    
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsetsMake(0, 20, 0, 20)
+        layout.itemSize = CGSize(width: itemSize, height: itemSize)
+        layout.minimumInteritemSpacing = 30
+        layout.minimumLineSpacing = 30
+        
+        collectionView.collectionViewLayout = layout
+        
         
         collectionView.delegate = self
         collectionView.dataSource = self
@@ -34,15 +50,26 @@ class GameViewController: UIViewController, UICollectionViewDelegate, UICollecti
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return images.count
+        return numOfRow * numOfCol
+        ///return images.count * 2
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customCell", for: indexPath) as! CustomCell
         
-        cell.myImage.image = UIImage(named: images[indexPath.row])
+        //cell.layer.cornerRadius = 50
+        cell.layer.borderColor = UIColor.lightGray.cgColor
+        cell.layer.borderWidth = 1
         
+        cell.myImage.image = UIImage(named: images[indexPath.row % 3])
+        
+        print(indexPath.row)
+
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Item pressed is: " , indexPath.row)
     }
 
 }
